6d71a4ee209f14a470a83f6ea7c425beee6f72dd
Sergio Jacome <sergio.jacome@gmail.com>
2017-12-12 17:32:36 -0500

    First commit

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../vendor/autoload.php';
+require '../src/config/db.php';
+
+$app = new \Slim\App;
+
+// Customer Routes
+require '../src/routes/customers.php';
+
+$app->run();
---
+++ src/routes/customers.php
@@ -0,0 +1,190 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+use Lcobucci\JWT\Builder;
+use Lcobucci\JWT\Signer\Hmac\Sha256;
+
+$signer = new Sha256();
+
+$token = (new Builder())->setIssuer('http://example.com') // Configures the issuer (iss claim)
+                        ->setAudience('http://example.org') // Configures the audience (aud claim)
+                        ->setId('4f1g23a12aa', true) // Configures the id (jti claim), replicating as a header item
+                        ->setIssuedAt(time()) // Configures the time that the token was issue (iat claim)
+                        ->setNotBefore(time() + 60) // Configures the time that the token can be used (nbf claim)
+                        ->setExpiration(time() + 3600) // Configures the expiration time of the token (exp claim)
+                        ->set('uid', 1) // Configures a new claim, called "uid"
+                        ->set('isthisok','yes')
+                        ->sign($signer, 'testing') // creates a signature using "testing" as key
+                        ->getToken(); // Retrieves the generated token
+$valid_token = $token->verify($signer, 'testing');
+print_r($valid_token);
+if($valid_token === true) {
+    echo('yes');
+} else {
+    echo('no');
+}
+
+$app = new \Slim\App;
+
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+});
+
+// Get All Customers
+$app->get('/api/customers', function(Request $request, Response $response, $valid_token){
+    if($valid_token === true) {
+        echo('<h1>yes</h1>');
+    } else {
+        echo('<h1>no</h1>');
+    }
+    $sql = "SELECT * FROM customers";
+
+    try{
+        // Get DB Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+
+        $stmt = $db->query($sql);
+        $customers = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+        echo json_encode($customers);
+    } catch(PDOException $e){
+        echo '{"error": {"text": '.$e->getMessage().'}';
+    }
+});
+
+// Get Single Customer
+$app->get('/api/customer/{id}', function(Request $request, Response $response){
+    $id = $request->getAttribute('id');
+
+    $sql = "SELECT * FROM customers WHERE id = $id";
+
+    try{
+        // Get DB Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+
+        $stmt = $db->query($sql);
+        $customer = $stmt->fetch(PDO::FETCH_OBJ);
+        $db = null;
+        echo json_encode($customer);
+    } catch(PDOException $e){
+        echo '{"error": {"text": '.$e->getMessage().'}';
+    }
+});
+
+// Add Customer
+$app->post('/api/customer/add', function(Request $request, Response $response){
+    $first_name = $request->getParam('first_name');
+    $last_name = $request->getParam('last_name');
+    $phone = $request->getParam('phone');
+    $email = $request->getParam('email');
+    $address = $request->getParam('address');
+    $city = $request->getParam('city');
+    $state = $request->getParam('state');
+
+    $sql = "INSERT INTO customers (first_name,last_name,phone,email,address,city,state) VALUES
+    (:first_name,:last_name,:phone,:email,:address,:city,:state)";
+
+    try{
+        // Get DB Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+
+        $stmt = $db->prepare($sql);
+
+        $stmt->bindParam(':first_name', $first_name);
+        $stmt->bindParam(':last_name',  $last_name);
+        $stmt->bindParam(':phone',      $phone);
+        $stmt->bindParam(':email',      $email);
+        $stmt->bindParam(':address',    $address);
+        $stmt->bindParam(':city',       $city);
+        $stmt->bindParam(':state',      $state);
+
+        $stmt->execute();
+
+        echo '{"notice": {"text": "Customer Added"}';
+
+    } catch(PDOException $e){
+        echo '{"error": {"text": '.$e->getMessage().'}';
+    }
+});
+
+// Update Customer
+$app->put('/api/customer/update/{id}', function(Request $request, Response $response){
+    $id = $request->getAttribute('id');
+    $first_name = $request->getParam('first_name');
+    $last_name = $request->getParam('last_name');
+    $phone = $request->getParam('phone');
+    $email = $request->getParam('email');
+    $address = $request->getParam('address');
+    $city = $request->getParam('city');
+    $state = $request->getParam('state');
+
+    $sql = "UPDATE customers SET
+				first_name 	= :first_name,
+				last_name 	= :last_name,
+                phone		= :phone,
+                email		= :email,
+                address 	= :address,
+                city 		= :city,
+                state		= :state
+			WHERE id = $id";
+
+    try{
+        // Get DB Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+
+        $stmt = $db->prepare($sql);
+
+        $stmt->bindParam(':first_name', $first_name);
+        $stmt->bindParam(':last_name',  $last_name);
+        $stmt->bindParam(':phone',      $phone);
+        $stmt->bindParam(':email',      $email);
+        $stmt->bindParam(':address',    $address);
+        $stmt->bindParam(':city',       $city);
+        $stmt->bindParam(':state',      $state);
+
+        $stmt->execute();
+
+        echo '{"notice": {"text": "Customer Updated"}';
+
+    } catch(PDOException $e){
+        echo '{"error": {"text": '.$e->getMessage().'}';
+    }
+});
+
+// Delete Customer
+$app->delete('/api/customer/delete/{id}', function(Request $request, Response $response){
+    $id = $request->getAttribute('id');
+
+    $sql = "DELETE FROM customers WHERE id = $id";
+
+    try{
+        // Get DB Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+
+        $stmt = $db->prepare($sql);
+        $stmt->execute();
+        $db = null;
+        echo '{"notice": {"text": "Customer Deleted"}';
+    } catch(PDOException $e){
+        echo '{"error": {"text": '.$e->getMessage().'}';
+    }
+});
